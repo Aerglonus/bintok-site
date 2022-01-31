6ec2bdafbbaf2d120f386421aca59060e8a0c390
Aerglonus <calebcontreras8@gmail.com>
2022-01-31 01:16:57 -0700

    changed site to valentines

---
+++ script.js
@@ -0,0 +1,29 @@
+function myFunction()
+   {
+    const newLocal = "fname";
+    let x = document.forms["myForm"][newLocal].value;
+    if (x == "definetly_binton","binton314","pie") {
+    window.location = "quest2.html"
+}
+    else {
+          alert("Wrong answer try again please");
+      return false; }
+
+   }
+
+   function secondFunction()
+   {
+    const newLocal = "dname";
+    let x = document.forms["myForm"][newLocal].value;
+    if (x == "What I want to say is that") {
+    window.location = "result.html"
+}
+    else {
+          alert("Wrong answer try again please");
+      return false; }
+
+   }
+
+   document.querySelector('body').addEventListener('contextmenu', (e) => {
+     e.preventDefault();
+   })
---
+++ txts/quest1.md
@@ -0,0 +1,15 @@
+<!-- ![Tux, the Linux mascot](/assets/img/nervous.jpg) -->
+<p align="center" style="margin-bottom: 0px !important;">
+  <img width="500" src="img/nervous.jpg"  align="center">
+</p>
+
+# You found the file link nice, now this is one of the secret words
+
+one correct answer is  `definetly_binton`
+
+But hey there’s more for the next quest, you're going to need this:
+
+V2hhdCBJIHdhbnQgdG8gc2F5IGlzIHRoYXQ
+
+put it here and summit the result :
+<https://codebeautify.org/base64-decode>
