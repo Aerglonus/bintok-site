3a6caa533770ce4d4ad7ef3498ea9ae5fe1fa048
Aerglonus <calebcontreras8@gmail.com>
2022-01-31 22:38:04 -0700

    star fix

--- script.js
+++ script.js
@@ -34,10 +34,10 @@ function myFunction()
 
  function createSnowFlake() {
 	const snow_flake = document.createElement('i');
-	snow_flake.classList.add('far');
+	snow_flake.classList.add('fas');
 	snow_flake.classList.add('fa-star');
 	snow_flake.style.left = Math.random() * window.innerWidth + 'px';
-	snow_flake.style.animationDuration = `${Math.random() * 5 + 2}s`; // between 2 - 5 seconds
+	snow_flake.style.animationDuration = `${Math.random() * 5 + 5}s`; // between 2 - 5 seconds
 	snow_flake.style.opacity = Math.random();
 	snow_flake.style.fontSize = `${Math.random() * 10 + 10}px`;
     snow_flake.style.transform=("rotate(45deg")
