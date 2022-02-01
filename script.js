f74bbc079d289333b851bd99a1a58474d6be11d3
Aerglonus <calebcontreras8@gmail.com>
2022-01-31 22:06:28 -0700

    added nft dave

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+setInterval(createSnowFlake, 600);
+
 function myFunction()
    {
     const newLocal = "fname";
@@ -27,3 +29,23 @@ function myFunction()
    document.querySelector('body').addEventListener('contextmenu', (e) => {
      e.preventDefault();
    })
+
+
+
+ function createSnowFlake() {
+	const snow_flake = document.createElement('i');
+	snow_flake.classList.add('far');
+	snow_flake.classList.add('fa-star');
+	snow_flake.style.left = Math.random() * window.innerWidth + 'px';
+	snow_flake.style.animationDuration = `${Math.random() * 5 + 2}s`; // between 2 - 5 seconds
+	snow_flake.style.opacity = Math.random();
+	snow_flake.style.fontSize = `${Math.random() * 10 + 10}px`;
+    snow_flake.style.transform=("rotate(45deg")
+	
+	document.body.appendChild(snow_flake);
+	
+	setTimeout(() => {
+		snow_flake.remove();
+	}, 5000)
+}
+
